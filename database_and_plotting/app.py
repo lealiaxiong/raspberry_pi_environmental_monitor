cb98b8bfe43057d6967e8a7eefe88e40043aed5d
Lealia Xiong <lxiong@caltech.edu>
2020-09-23 09:09:05 -0700

    Update app.py to embed Bokeh server.

--- database_and_plotting/app.py
+++ database_and_plotting/app.py
@@ -1,7 +1,7 @@
 import time
 import datetime
 import sqlite3
-from threading import Thread
+from multiprocessing import Process
 
 import numpy as np
 from bokeh.embed import server_document
@@ -80,12 +80,12 @@ def read_sensors():
             conn.commit()
             conn.close()
             
-@app.route('/_get_last_data')
+
 def get_last_data():
     conn = sqlite3.connect(db)
     curs = conn.cursor()
     for row in curs.execute("SELECT * FROM data ORDER BY timestamp DESC LIMIT 1"):
-        now_string = row[0]#.strftime("%Y-%m-%d %H:%M:%S")
+        now_string = row[0]
         temp_data = row[1]
         humid_data = row[2]
         pressure_data = row[3]
@@ -93,15 +93,98 @@ def get_last_data():
         tvoc_data = row[5]
         light_data = row[6]
         uv_data = row[7]
+    
     conn.close()
+    
+    return now_string, temp_data, humid_data, pressure_data, eCO2_data, tvoc_data, light_data, uv_data
+
+@app.route('/_get_last_data')
+def show_last_data():
+    now_string, temp_data, humid_data, pressure_data, eCO2_data, tvoc_data, light_data, uv_data = get_last_data()
     return jsonify(temp_data=temp_data, humid_data=humid_data, pressure_data=pressure_data, eCO2_data=eCO2_data,
     tvoc_data=tvoc_data, light_data=light_data, uv_data=uv_data, now=now_string)
+    
+def get_num_datapoints(db):
+    conn = sqlite3.connect(db)
+    curs = conn.cursor()
+    
+    for row in curs.execute("SELECT COUNT(timestamp) FROM data"):
+        num_datapoints = row[0]
+    
+    conn.close()
+    
+    return num_datapoints
+
+def bkapp(doc):
+    num_datapoints = get_num_datapoints(db)
+    rollover = 300
+
+    conn = sqlite3.connect("environmental_monitor.db")
+    curs = conn.cursor()
+    
+    curs.execute("SELECT * FROM data ORDER BY timestamp DESC LIMIT " 
+    + str(min(rollover, num_datapoints)))
+
+    db_data = curs.fetchall()
+    
+    datatype_names = ("timestamps", "temps", "humidities", "pressures", "eCO2s", "tvocs", "lights", "uvs")
+    data_lists = ([] for name in datatype_names)
+
+    data = dict(zip(datatype_names, data_lists))
+    
+    for row in reversed(db_data):
+        for datatype, datapoint in zip(data, row):
+            data[datatype].append(datapoint)
+            
+    for i, timestamp in enumerate(data['timestamps']):
+        data['timestamps'][i] = datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+        
+    source = bokeh.models.ColumnDataSource(data=data)
+    
+    p_light = bokeh.plotting.figure(
+        frame_width=500,
+        frame_height=175,
+        x_axis_label="time",
+        y_axis_label="light (lux)",
+        x_axis_type="datetime"
+    )
+    p_light.xaxis.formatter=bokeh.models.DatetimeTickFormatter(
+        seconds = ['%H:%M:%S'],
+        minutes = ['%H:%M:%S'],
+        hourmin = ['%H:%M:%S'],
+        days = ['%Y-%m-%d %H:%M:%S'],
+        months = ['%Y-%m-%d %H:%M:%S'],
+    )
+    p_light.x_range.range_padding = 0
+    
+    light_line = p_light.line(source=source, x="timestamps", y="lights")
+    
+    @bokeh.driving.linear()
+    def update(step):
+        last_data = [[data] for data in get_last_data()]
+        update_dict = dict(zip(datatype_names, last_data))
+        timestamp = update_dict['timestamps'][0]
+        update_dict['timestamps'][0] = datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+        source.stream(update_dict, rollover)
+        
+    pc = doc.add_periodic_callback(update, sample_frequency*1000)
+        
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    script = server_document("http://localhost:5006/bkapp")
+    return render_template('index.html', script=script, template="Flask")
+    
+def bk_worker():
+    server = Server({'/bkapp': bkapp}, io_loop=IOLoop(), allow_websocket_origin=["localhost:5000"])
+    server.start()
+    server.io_loop.start()
 
-Thread(target=read_sensors).start()
+process_read = Process(target=read_sensors)
+process_plot = Process(target=bk_worker)
+
+process_read.start()
+process_plot.start()
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0', port=5000)
